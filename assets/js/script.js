a6007a27573750ae70af25b99d445c59f4cc38f5
raul <vrap3@hotmail.com>
2019-12-03 07:45:03 -0300

    primer avanbce pagina curriculum online

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function(){
+        window.location.hash = gato;
+      });
+    }
+  });
+
+  $('[data-toggle="popover"]').popover();
+
+});
